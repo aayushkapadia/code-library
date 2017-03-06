d68bb297cd6b8dc2385f1506bb3f7f24cd36df1e
Aayush Kapadia <kapadiaaayush@gmail.com>
2017-03-07 00:56:10 +0530

    Created and Implement MyScannerTest

---
+++ JAVA/Test/IO/MyScannerTest.java
@@ -0,0 +1,18 @@
+package Test.IO;
+
+import Source.IO.MyScanner;
+
+public class MyScannerTest
+{
+	public static void main(String[] args)
+	{
+		MyScanner sc = new MyScanner();
+		System.out.println(sc.nextLine());
+		System.out.println(sc.next());
+		System.out.println(sc.nextInt());
+		System.out.println(sc.nextDouble());
+		System.out.println(sc.nextLine());
+		System.out.println(sc.next());
+		System.out.println(sc.nextLong());
+	}
+}
