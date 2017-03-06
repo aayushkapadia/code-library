119d494bafb2acdb3e1113ca6e6ae52105cb3845
Aayush Kapadia <kapadiaaayush@gmail.com>
2017-03-07 00:07:12 +0530

    Implemented MyScanner Class

---
+++ JAVA/Source/IO/MyScanner.java
@@ -0,0 +1,75 @@
+import java.util.*;
+import java.io.*;
+
+public class MyScanner
+{
+	private BufferedReader br;
+	private StringTokenizer st;
+
+	public MyScanner()
+	{
+		br = new BufferedReader(new InputStreamReader(System.in));
+		st = null;
+	}
+
+	public String next()
+	{
+		while(st==null || (!st.hasMoreTokens()))
+		{
+			try
+			{
+				st = new StringTokenizer(br.readLine());
+			}
+			catch(IOException e)
+			{
+				e.printStackTrace();
+			}
+		}
+
+		return st.nextToken();
+	}
+
+	public int nextInt()
+	{
+		return Integer.parseInt(next());
+	}
+
+	public long nextLong()
+	{
+		return Long.parseLong(next());
+	}
+
+	public double nextDouble()
+	{
+		return Double.parseDouble(next());
+	}
+
+	public String nextLine()
+	{
+		String str = "";
+		try 
+		{
+	    	str = br.readLine();
+	  	} 
+	  	catch (IOException e) 
+	  	{
+	    	e.printStackTrace();
+	  	}
+	  	return str;
+	}
+
+	public void close()
+	{
+		try
+		{
+			br.close();
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+		
+		st = null;
+	}
+
+}
