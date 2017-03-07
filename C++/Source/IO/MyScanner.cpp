1b9107fc9cc31286e1f0ff78b42a1b7bc6036a05
Aayush Kapadia <kapadiaaayush@gmail.com>
2017-03-07 23:47:37 +0530

    Implemented MyScanner and MyScannerTest for C++

--- C++/Source/IO/MyScanner.cpp
+++ C++/Source/IO/MyScanner.cpp
@@ -1,42 +1,57 @@
 #include<iostream>
+#include<string>
+#include<algorithm>
+#include<limits>
+#include "MyScanner.h"
+
 using namespace std;
 
-class MyScanner
+MyScanner::MyScanner()
 {
-public:
-	MyScanner()
-	{
-		std::ios::sync_with_stdio(false);
-		cin.tie(NULL);
-	}
-	string next()
-	{
-		string x;
-		cin>>x;
-		return x;
-	}
-	string nextLine()
-	{
-		string x;
-		getline(cin,x);
-		return x;
-	}
-	int nextInt()
-	{
-		int x;
-		cin>>x;
-		return x;
-	}
-	long long int nextLong()
-	{
-		long long int x;
-		cin>>x;
-		return x;
-	}
-	double nextDouble()
-	{
-		double x;
-		cin>>x;
-		return x;
-	}
-};
+	std::ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	newLineScanned = true;
+}
+string MyScanner::next()
+{
+	newLineScanned = false;
+
+	string x;
+	cin>>x;
+	return x;
+}
+string MyScanner::nextLine()
+{
+	if(!newLineScanned)
+		cin.ignore(numeric_limits<std::streamsize>::max(), '\n');		
+
+	newLineScanned = true;
+
+	string x;
+	getline(cin,x);
+	return x;
+}
+int MyScanner::nextInt()
+{
+	newLineScanned = false;
+
+	int x;
+	cin>>x;
+	return x;
+}
+long long int MyScanner::nextLong()
+{
+	newLineScanned = false;
+
+	long long int x;
+	cin>>x;
+	return x;
+}
+double MyScanner::nextDouble()
+{
+	newLineScanned = false;
+
+	double x;
+	cin>>x;
+	return x;
+}
--- C++/Source/IO/MyScanner.h
+++ C++/Source/IO/MyScanner.h
@@ -1,5 +1,10 @@
+#include<string>
+using namespace std;
+
 class MyScanner
 {
+private:
+	bool newLineScanned;	// Indicates whether new line character of previous line scanned. Only checked when using nextLine()
 public:
 	MyScanner();
 	string next();
