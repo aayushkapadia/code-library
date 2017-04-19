83b6aec5f29f1456c094a32fb3b6a541b04d456d
Aayush Kapadia <kapadiaaayush@gmail.com>
2017-04-19 19:45:10 +0530

    Add documrntation of Union Find Class

--- C++/Source/Graph/UnionFind.h
+++ C++/Source/Graph/UnionFind.h
@@ -1,6 +1,11 @@
 #include<vector>
 using namespace std;
 
+/*
+	This class divides the vertices from 1 to n into several components
+	It will keep track of which vertice is in which component
+	Initially all the vertices will be in there respective component
+*/
 class UnionFind
 {
 private:
@@ -8,10 +13,38 @@ private:
 	int *size1;	
 	int n;		
 public:
+	/*
+		Constrct object of this class
+		@param n : The number of vertices you want to keep track of
+	*/
 	UnionFind(int n);
+
+	/*
+		Combines component of b into component of a
+		@param a,b The two vertices whose components you want to combine
+		@return True if two vertices were initially in two different components 
+				False if two components were initially in the same component
+	*/
 	bool join(int a,int b);
+
+	/*
+		Checks if both the vertices belongs to the same component
+		@ param a,b The two vertices
+		@ return True if belongs to the same component
+				 False if belongs to different component
+	*/
 	bool areConnected(int a,int b);
+
+	/*
+		Returns the size of the component of vertice a
+		@ param a : The vertice
+	*/
 	int getSize(int a);
+
+	/*
+		Returns vector of all components formed. A Individual Component 
+		is also a vector of ints indicating the vertices belonging to that component
+	*/
 	vector<vector<int> > getComponents();
 private:
 	int getParent(int i);
--- JAVA/Source/Graph/UnionFind.java
+++ JAVA/Source/Graph/UnionFind.java
@@ -2,11 +2,19 @@ package Source.Graph;
 
 import java.util.*;
 
+/*	This class divides the vertices from 1 to n into several components
+	It will keep track of which vertice is in which component
+	Initially all the vertices will be in there respective component
+*/
 public class UnionFind
 {
 	private int[] parent;
 	private int[] size;
 
+	/*
+		Constrct object of this class
+		@param n : The number of vertices you want to keep track of
+	*/
 	public UnionFind(int n)
 	{
 		parent = new int[n];
@@ -38,6 +46,12 @@ public class UnionFind
 		return curr;
 	}
 
+	/*
+		Combines component of b into component of a
+		@param a,b The two vertices whose components you want to combine
+		@return True if two vertices were initially in two different components 
+				False if two components were initially in the same component
+	*/
 	public boolean join(int a,int b)
 	{
 		int parA = getParent(a);
@@ -53,6 +67,12 @@ public class UnionFind
 		}
 	}
 
+	/*
+		Checks if both the vertices belongs to the same component
+		@ param a,b The two vertices
+		@ return True if belongs to the same component
+				 False if belongs to different component
+	*/
 	public boolean areConnected(int a,int b)
 	{
 		int parA = getParent(a);
@@ -61,12 +81,20 @@ public class UnionFind
 		return parA==parB;
 	}
 
+	/*
+		Returns the size of the component of vertice a
+		@ param a : The vertice
+	*/
 	public int getSize(int a)
 	{
 		int parA = getParent(a);
 		return size[parA];
 	}
 
+	/*
+		Returns ArrayList of all components formed. A Individual Component 
+		is also a ArrayList of Integer indicating the vertices belonging to that component
+	*/
 	public ArrayList<ArrayList<Integer>> getComponents()
 	{
 		ArrayList<ArrayList<Integer>> components = new ArrayList<ArrayList<Integer>>();
