9f3fd352782a88846f67c89d6306cfa5434d8a69
Aayush Kapadia <kapadiaaayush@gmail.com>
2017-04-19 19:20:41 +0530

    Add Dumping Feature in nextLine() method and add documentation

--- C++/Source/IO/MyScanner.h
+++ C++/Source/IO/MyScanner.h
@@ -1,15 +1,38 @@
 #include<string>
 using namespace std;
 
+/*
+	This class is used to scan data from standard IO stream
+*/
 class MyScanner
 {
 private:
 	bool newLineScanned;	// Indicates whether new line character of previous line scanned. Only checked when using nextLine()
 public:
 	MyScanner();
+	/*
+		Reads next token as a string from the data
+	*/
 	string next();
+
+	/*
+		Reads next line as a whole from the data. If 1 or more tokens are already read from the current line
+		and it contains few more tokens then it will dump all the tokens on current line and return just the next line after it
+	*/
 	string nextLine();
+
+	/*
+		Reads next token as a int from the data
+	*/
 	int nextInt();
+
+	/*
+		Reads next token as a long long int from the data
+	*/
 	long long int nextLong();
+
+	/*
+		Reads next token as a double from the data
+	*/
 	double nextDouble();
 };
--- JAVA/Source/IO/MyScanner.java
+++ JAVA/Source/IO/MyScanner.java
@@ -14,6 +14,9 @@ public class MyScanner
 		st = null;
 	}
 
+	/*
+		Reads next token from the data
+	*/
 	public String next()
 	{
 		while(st==null || (!st.hasMoreTokens()))
@@ -31,23 +34,38 @@ public class MyScanner
 		return st.nextToken();
 	}
 
+	/*
+		Reads next token of the data as a int
+	*/
 	public int nextInt()
 	{
 		return Integer.parseInt(next());
 	}
 
+	/*
+		Reads next token of the data as a long
+	*/
 	public long nextLong()
 	{
 		return Long.parseLong(next());
 	}
 
+	/*
+		Reads next token of the data as a double
+	*/
 	public double nextDouble()
 	{
 		return Double.parseDouble(next());
 	}
 
+	/*
+		Reads next line as a whole from the data. If one or more tokens are 
+		already read from the current line and some are still left to be read then
+		it will dump all the remaining tokens from the current line and read the just next line after this
+	*/
 	public String nextLine()
 	{
+		st = null;
 		String str = "";
 		try 
 		{
@@ -60,6 +78,9 @@ public class MyScanner
 	  	return str;
 	}
 
+	/*
+		Frees all the resources used by this class
+	*/
 	public void close()
 	{
 		try
